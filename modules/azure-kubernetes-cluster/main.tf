5b4a9803cbebf0ca8dc1d40e92de39a1f142a3cf
Deepanshu Marwah <deepanshumarwah@gmail.com>
2020-07-20 12:46:28 +0800

    Remove additional comments from main.tf (patch)

--- modules/azure-kubernetes-cluster/main.tf
+++ modules/azure-kubernetes-cluster/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "K8s" {
   resource_group_name     = var.resource_group_name
   tags                    = var.tags
   kubernetes_version      = var.kubernetes_version
-  private_cluster_enabled = var.private_cluster_enabled #possibly needed for api to be exposed only to internal IPs
+  private_cluster_enabled = var.private_cluster_enabled
   dns_prefix              = var.dns_prefix
 
   identity {
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "K8s" {
   }
 
   addon_profile {
-    oms_agent { #required for monitoring K8s cluster by sentinel
+    oms_agent {
       enabled                    = var.oms_agent_enabled
       log_analytics_workspace_id = var.log_analytics_workspace_id
     }
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "K8s" {
   default_node_pool {
     name                = var.node_name
     vm_size             = var.vm_size
-    enable_auto_scaling = var.enable_auto_scaling #if false then node_count, else min-max count
+    enable_auto_scaling = var.enable_auto_scaling
     node_count          = var.node_count
     vnet_subnet_id      = var.vnet_subnet_id
     os_disk_size_gb     = var.os_disk_size_gb

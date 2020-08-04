7e2f25048f1a745f46db6c543de93b34835252e4
Luke Fritz <lukefritz@gmail.com>
2020-08-04 00:41:57 -0500

    Update output descriptions; make client key sensitive (fix)

--- modules/azure-kubernetes-cluster/outputs.tf
+++ modules/azure-kubernetes-cluster/outputs.tf
@@ -14,17 +14,22 @@ output "kube_config_raw" {
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
+  description = "The private key corresponding to the provisioned client certificate."
+  value       = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
+  sensitive   = true
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+  description = "The client certificate used to access the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
+  description = "The Kubernetes cluster's root CA certificate."
+  value       = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  description = "The hostname used to access the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.cluster.kube_config.0.host
 }

eab8a28656d978dc4c5fbcad6803b4a0b935afd9
Luke Fritz <lukefritz@gmail.com>
2020-08-25 16:23:44 -0500

    Use unique name for cluster resource group (fix)

--- modules/azure-kubernetes-cluster/main.tf
+++ modules/azure-kubernetes-cluster/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   name                    = var.name
   location                = var.location
   resource_group_name     = var.resource_group_name
-  node_resource_group     = var.resource_group_name
+  node_resource_group     = "${var.resource_group_name}-cluster-resources"
   tags                    = var.tags
   kubernetes_version      = var.kubernetes_version
   private_cluster_enabled = var.private_cluster_enabled
